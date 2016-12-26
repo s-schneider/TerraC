5e8525b14830d23e0e15f29b2a78e838a745d570
s-schneider <simon.schneider@wwu.de>
2016-12-26 19:48:30 +0100

    fixed error in search engine

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
 	has_many :receipts
 
 	def self.search(search)
-		where('customer_name LIKE :search OR customer_surname LIKE :search OR customer_street LIKE :search OR customer_town LIKE ?', search: "%#{search}%")
+		where('customer_name LIKE :search OR customer_surname LIKE :search OR customer_street LIKE :search OR customer_town LIKE :search', search: "%#{search}%")
 	end
 
 	def full_name
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -7,7 +7,7 @@ class Receipt < ApplicationRecord
 	scope :state, -> (status) {where status: status}
 
 	def self.search(search)
-		where('receipt_number LIKE :search OR receipt_type LIKE :search OR receipt_person LIKE :search OR producer LIKE :search OR article LIKE ? or status LIKE :search', search: "%#{search}%")
+		where('receipt_number LIKE :search OR receipt_type LIKE :search OR receipt_person LIKE :search OR producer LIKE :search OR article LIKE :search or status LIKE :search', search: "%#{search}%")
 	end
 
 end
