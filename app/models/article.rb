70f31f3f7ab6755897fcd5bd6039ad7257244825
s-schneider <simon.schneider@wwu.de>
2016-12-27 16:50:18 +0100

    startet working on receipt-supplier-article modell with mutliple belongs_to

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+	has_many :receipts
+
 
 	def self.search(search)
 		where('article_name LIKE :search OR article_producer LIKE :search OR article_color LIKE :search OR article_size LIKE :search', search: "%#{search}%")
